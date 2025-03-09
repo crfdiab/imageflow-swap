4266ed903c3a5fa3fbf87ae16c411553e4591444
crfdiab <trickyaffiliate.com@gmail.com>
2025-03-09 23:06:50 +0200

    New Commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -28,13 +27,13 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/:slug" element={<Index />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/data-protection" element={<DataProtection />} />
           <Route path="/terms" element={<Terms />} />
+          <Route path="/" element={<Index />} />
+          <Route path="/:slug" element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
